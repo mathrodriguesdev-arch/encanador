506982ad207415e1fa65f94595020f78ca246272
mathrodriguesdev-arch <mathrodrigues.dev@gmail.com>
2026-03-29 02:15:45 -0300

    Projeto para encanadores

---
+++ script.js
@@ -0,0 +1,9 @@
+const formulario = document.querySelector(".formulario-fale-conosco")
+
+function mostrarForm() {
+    formulario.classList.add("mostrar-form")
+}
+
+function esconderForm() {
+    formulario.classList.remove("mostrar-form")
+}
